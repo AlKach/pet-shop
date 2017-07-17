630cb8b429bbc56170c81b474fa700e8d4460a12
Aliaksandr Kachanov <Aliaksandr_Kachanov@epam.com>
2017-07-17 15:38:06 +0300

    Add tests for converters

---
+++ src/test/java/by/kachanov/shop/service/converter/ConverterInstantiationTest.java
@@ -0,0 +1,45 @@
+package by.kachanov.shop.service.converter;
+
+import by.kachanov.shop.SpringTest;
+import org.hibernate.criterion.Criterion;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ConverterInstantiationTest extends SpringTest {
+
+    @Autowired
+    private List<ContextAwareConverter> converters;
+
+    @Test
+    public void testConverterInstantiation() {
+        for (ContextAwareConverter converter : converters) {
+            try {
+                ContextAwareConverter newConverter = converter.getClass().newInstance();
+                assertNotNull(newConverter);
+            } catch (InstantiationException | IllegalAccessException e) {
+                fail(converter.getClass().getName() + " can't be instantiated by default constructor");
+            }
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testUninstantiatableConverter() {
+        ContextAwareConverter uninstantiatableConverter = new UninstantiatableConverter();
+        uninstantiatableConverter.withContext(new ConversionContext(null));
+    }
+
+    private static class UninstantiatableConverter extends AbstractConditionConverter {
+
+        private UninstantiatableConverter() {}
+
+        @Override
+        protected Criterion doConvert(Object source) {
+            return null;
+        }
+    }
+
+}

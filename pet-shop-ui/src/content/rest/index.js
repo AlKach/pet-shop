11d74defd845942df2f1cd5af389e8772e6da78b
Aliaksandr Kachanov <Aliaksandr_Kachanov@epam.com>
2019-01-05 16:16:04 +0300

    Serve static content with Nginx

--- pet-shop-ui/src/content/rest/index.js
+++ pet-shop-ui/src/content/rest/index.js
@@ -3,24 +3,30 @@ import 'swagger-ui-dist/swagger-ui.css';
 
 document.addEventListener('DOMContentLoaded',
 	function() {
-        window.ui = SwaggerUIBundle({
-            url: "api-docs",
-            dom_id: '#swagger-ui',
-            deepLinking: true,
-            defaultModelsExpandDepth: -1,
-            displayRequestDuration: true,
-            docExpansion: 'none',
-            filter: true,
-            tagsSorter: 'alpha',
-            validatorUrl: null,
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-            ],
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
-            layout: "StandaloneLayout"
-        });
+        fetch('api-docs')
+            .then(spec => spec.text())
+            .then(spec => JSON.parse(spec))
+            .then(spec => {
+                spec.host = window.location.host;
+                window.ui = SwaggerUIBundle({
+                    spec: spec,
+                    dom_id: '#swagger-ui',
+                    deepLinking: true,
+                    defaultModelsExpandDepth: -1,
+                    displayRequestDuration: true,
+                    docExpansion: 'none',
+                    filter: true,
+                    tagsSorter: 'alpha',
+                    validatorUrl: null,
+                    presets: [
+                        SwaggerUIBundle.presets.apis,
+                        SwaggerUIStandalonePreset
+                    ],
+                    plugins: [
+                        SwaggerUIBundle.plugins.DownloadUrl
+                    ],
+                    layout: "StandaloneLayout"
+                });
+            });
 	}
 );
